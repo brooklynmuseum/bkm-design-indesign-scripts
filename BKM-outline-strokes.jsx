9341fbbb7bc9ea8d474402db7ca8d601bf1cf59b
Adam O'Reilly <adamtaylororeilly@gmail.com>
2023-08-22 17:16:08 -0400

    Update BKM-outline-strokes.jsx

--- BKM-outline-strokes.jsx
+++ BKM-outline-strokes.jsx
@@ -1,7 +1,27 @@
 if (app.documents.length > 0) {
     var doc = app.activeDocument;
 
-    // Function to outline strokes for a collection of page items
+    // Function to ungroup items
+    function ungroupItems(pageItemCollection) {
+        for (var j = pageItemCollection.length - 1; j >= 0; j--) {
+            var item = pageItemCollection[j];
+            if (item.constructor.name === "Group") {
+                item.ungroup();
+            }
+        }
+    }
+
+    // Function to convert text frames to outlines
+    function convertTextFramesToOutlines(pageItemCollection) {
+        for (var j = 0; j < pageItemCollection.length; j++) {
+            var item = pageItemCollection[j];
+            if (item instanceof TextFrame) {
+                item.createOutlines();
+            }
+        }
+    }
+
+    // Function to outline strokes
     function outlineStrokes(pageItemCollection) {
         for (var j = 0; j < pageItemCollection.length; j++) {
             var item = pageItemCollection[j];
@@ -11,20 +31,26 @@ if (app.documents.length > 0) {
         }
     }
 
-    // Outline strokes in document pages
+    // Process for document pages
     for (var i = 0; i < doc.pages.length; i++) {
-        outlineStrokes(doc.pages[i].allPageItems);
+        var page = doc.pages[i];
+        ungroupItems(page.allPageItems);
+        convertTextFramesToOutlines(page.textFrames);
+        outlineStrokes(page.allPageItems);
     }
 
-    // Outline strokes in master pages
+    // Process for master pages
     for (var k = 0; k < doc.masterSpreads.length; k++) {
         var masterSpread = doc.masterSpreads[k];
         for (var m = 0; m < masterSpread.pages.length; m++) {
-            outlineStrokes(masterSpread.pages[m].allPageItems);
+            var masterPage = masterSpread.pages[m];
+            ungroupItems(masterPage.allPageItems);
+            convertTextFramesToOutlines(masterPage.textFrames);
+            outlineStrokes(masterPage.allPageItems);
         }
     }
 
-    alert("Strokes converted to outlines successfully!");
+    alert("Processing completed!");
 } else {
     alert("No active document found. Please open a document and try again.");
 }
